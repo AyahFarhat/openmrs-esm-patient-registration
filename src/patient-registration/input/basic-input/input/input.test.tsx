f65639653e93b428a315784e0bca60e8cce19c62
AyahFarhat <97855138+AyahFarhat@users.noreply.github.com>
2024-09-26 17:48:49 -0400

    Update input.test.tsx

--- src/patient-registration/input/basic-input/input/input.test.tsx
+++ src/patient-registration/input/basic-input/input/input.test.tsx
@@ -3,165 +3,93 @@ import { render, fireEvent, wait, screen } from '@testing-library/react';
 import { Formik, Form } from 'formik';
 import { Input } from './input.component';
 
-describe.skip('number input', () => {
-  const setupInput = async () => {
-    render(
-      <Formik initialValues={{ number: 0 }} onSubmit={null}>
-        <Form>
-          <Input id="number" labelText="Number" name="number" light />
-        </Form>
-      </Formik>,
-    );
-    return screen.getByLabelText('number') as HTMLInputElement;
-  };
+const setupInput = async (initialValues: object, inputProps: any) => {
+  render(
+    <Formik initialValues={initialValues} onSubmit={null}>
+      <Form>
+        <Input {...inputProps} light />
+      </Form>
+    </Formik>,
+  );
+  return screen.getByLabelText(inputProps.labelText) as HTMLInputElement;
+};
 
-  it('exists', async () => {
-    const input = await setupInput();
-    expect(input.type).toEqual('number');
+const testInputExists = async (type: string, initialValues: object, inputProps: any) => {
+  const input = await setupInput(initialValues, inputProps);
+  expect(input.type).toEqual(type);
+};
+
+const testInputCanInputData = async (expected: any, initialValues: object, inputProps: any, field: string, valueKey: string) => {
+  const input = await setupInput(initialValues, inputProps);
+
+  fireEvent.change(input, { target: { [valueKey]: expected } });
+  fireEvent.blur(input);
+
+  await wait();
+
+  expect(input[field]).toEqual(expected);
+};
+
+describe.skip('inputs', () => {
+  it('number input exists', async () => {
+    await testInputExists('number', { number: 0 }, { 
+      id: 'number', labelText: 'Number', name: 'number' });
   });
 
-  it('can input data', async () => {
-    const input = await setupInput();
-    const expected = 1;
-
-    fireEvent.change(input, { target: { valueAsNumber: expected } });
-    fireEvent.blur(input);
-
-    await wait();
-
-    expect(input.valueAsNumber).toEqual(expected);
-  });
-});
-
-describe.skip('text input', () => {
-  const setupInput = async () => {
-    render(
-      <Formik initialValues={{ text: '' }} onSubmit={null}>
-        <Form>
-          <Input id="text" labelText="Text" name="text" placeholder="Enter text" light />
-        </Form>
-      </Formik>,
-    );
-    return screen.getByLabelText('text') as HTMLInputElement;
-  };
-
-  it('exists', async () => {
-    const input = await setupInput();
-    expect(input.type).toEqual('text');
+  it('number input can input data', async () => {
+    await testInputCanInputData(1, { number: 0 }, 
+      {id: 'number', labelText: 'Number', name: 'number' }, 'valueAsNumber', 'valueAsNumber');
   });
 
-  it('can input data', async () => {
-    const input = await setupInput();
-    const expected = 'Some text';
-
-    fireEvent.change(input, { target: { value: expected } });
-    fireEvent.blur(input);
-
-    await wait();
-
-    expect(input.value).toEqual(expected);
-  });
-});
-
-describe.skip('telephone number input', () => {
-  const setupInput = async () => {
-    render(
-      <Formik initialValues={{ telephoneNumber: '' }} onSubmit={null}>
-        <Form>
-          <Input
-            id="tel"
-            labelText="Telephone Number"
-            name="telephoneNumber"
-            placeholder="Enter telephone number"
-            light
-          />
-        </Form>
-      </Formik>,
-    );
-    return screen.getByLabelText('telephoneNumber') as HTMLInputElement;
-  };
-
-  it('exists', async () => {
-    const input = await setupInput();
-    expect(input.type).toEqual('tel');
+  it('text input exists', async () => {
+    await testInputExists('text', { text: '' },
+      { id: 'text', labelText: 'Text', name: 'text', placeholder: 'Enter text' });
   });
 
-  it('can input data', async () => {
-    const input = await setupInput();
-    const expected = '0800001066';
-
-    fireEvent.change(input, { target: { value: expected } });
-    fireEvent.blur(input);
-
-    await wait();
-
-    expect(input.value).toEqual(expected);
-  });
-});
-
-describe.skip('date input', () => {
-  const setupInput = async () => {
-    render(
-      <Formik initialValues={{ date: '' }} onSubmit={null}>
-        <Form>
-          <Input id="date" labelText="date" name="date" light />
-        </Form>
-      </Formik>,
-    );
-    return screen.getByLabelText('date') as HTMLInputElement;
-  };
-
-  it('exists', async () => {
-    const input = await setupInput();
-    expect(input.type).toEqual('date');
+  it('text input can input data', async () => {
+    await testInputCanInputData('Some text', { text: '' }, 
+      { id: 'text', labelText: 'Text', name: 'text', placeholder: 'Enter text' }, 'value', 'value');
   });
 
-  it('can input data', async () => {
-    const input = await setupInput();
-    const expected = '1990-09-10';
-
-    fireEvent.change(input, { target: { value: expected } });
-    fireEvent.blur(input);
-
-    await wait();
-
-    expect(input.value).toEqual(expected);
-  });
-});
-
-describe.skip('checkbox input', () => {
-  const setupInput = async () => {
-    render(
-      <Formik initialValues={{ checkbox: false }} onSubmit={null}>
-        <Form>
-          <Input id="checkbox" labelText="checkbox" name="checkbox" light />
-        </Form>
-      </Formik>,
-    );
-    return screen.getByLabelText('checkbox') as HTMLInputElement;
-  };
-
-  it('exists', async () => {
-    const input = await setupInput();
-    expect(input.type).toEqual('checkbox');
+  it('telephone input exists', async () => {
+    await testInputExists('tel', { telephoneNumber: '' }, 
+      { id: 'tel', labelText: 'Telephone Number', name: 'telephoneNumber', placeholder: 'Enter telephone number' });
   });
 
-  it('can input data', async () => {
-    const input = await setupInput();
-    const expected = true;
+  it('telephone input can input data', async () => {
+    await testInputCanInputData('0800001066', { telephoneNumber: '' }, 
+      { id: 'tel', labelText: 'Telephone Number', name: 'telephoneNumber', placeholder: 'Enter telephone number' }, 'value', 'value');
+  });
 
+  it('date input exists', async () => {
+    await testInputExists('date', { date: '' }, 
+      { id: 'date', labelText: 'Date', name: 'date' });
+  });
+
+  it('date input can input data', async () => {
+    await testInputCanInputData('1990-09-10', { date: '' }, 
+      { id: 'date', labelText: 'Date', name: 'date' }, 'value', 'value');
+  });
+
+  it('checkbox input exists', async () => {
+    await testInputExists('checkbox', { checkbox: false }, 
+      { id: 'checkbox', labelText: 'Checkbox', name: 'checkbox' });
+  });
+
+  it('checkbox input can input data', async () => {
+    const input = await setupInput({ checkbox: false }, 
+      { id: 'checkbox', labelText: 'Checkbox', name: 'checkbox' });
     fireEvent.click(input);
     fireEvent.blur(input);
 
     await wait();
 
-    expect(input.checked).toEqual(expected);
+    expect(input.checked).toEqual(true);
   });
 
-  it('can update data', async () => {
-    const input = await setupInput();
-    const expected = false;
-
+  it('checkbox input can update data', async () => {
+    const input = await setupInput({ checkbox: false }, 
+      { id: 'checkbox', labelText: 'Checkbox', name: 'checkbox' });
     fireEvent.click(input);
     fireEvent.blur(input);
 
@@ -169,7 +97,7 @@ describe.skip('checkbox input', () => {
     fireEvent.blur(input);
 
     await wait();
-
-    expect(input.checked).toEqual(expected);
+    
+    expect(input.checked).toEqual(false);
   });
 });
